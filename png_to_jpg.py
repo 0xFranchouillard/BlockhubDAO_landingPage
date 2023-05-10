d2559e632c4ccd0b01dc254ed1543d2a369e2192
Franchouillard <cyrillechampion13@gmail.com>
2023-05-10 09:58:54 +0200

    fix tentative react component version conflict

--- png_to_jpg.py
+++ png_to_jpg.py
@@ -5,7 +5,7 @@ def png_to_jpg(imagefrom, to):
         new_img.paste(img, mask=img.split()[3])
         new_img.save(to, 'JPEG')
 
-png_to_jpg('public/PepeSilver.png', 'public/PepeSilver.jpg')
-png_to_jpg('public/PepePurple.png', 'public/PepePurple.jpg')
+png_to_jpg('public/images/PepeSilver.png', 'public/images/PepeSilver.jpg')
+png_to_jpg('public/images/PepePurple.png', 'public/images/PepePurple.jpg')
 if __name__ == '__main__':
     pass
