ae427f4653340b126cb8e6914ead0f788314e4c5
Famacito76 <louis.pn76@gmail.com>
2024-03-06 22:03:58 +0100

    MAJ:Front

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import Home from '../src/component/Home/Index'
-import Navbar from '../src/component/Home/Navbar'
 
 export default function Index() {
     return (
--- src/component/Home/ImageSlicer.js
+++ src/component/Home/ImageSlicer.js
@@ -1,9 +1,19 @@
-import React, { useState, FC } from 'react';
-import { ReactCompareSlider, ReactCompareSliderImage } from 'react-compare-slider';
-
+import React, { useState, useEffect } from 'react';
+import { ReactCompareSlider } from 'react-compare-slider';
 
 export default function ImageSlicer() {
     const [sliderPosition, setSliderPosition] = useState(50);
+
+    useEffect(() => {
+        const handleContainer = document.querySelector('[data-rcs="handle-container"]');
+        if (handleContainer) {
+            handleContainer.style.zIndex = 1000;
+        }
+    }, []);
+
+    const handleSliderChange = newPosition => {
+        setSliderPosition(newPosition);
+    };
     const CustomHandle = () => {
         return (
             <div
@@ -19,96 +29,105 @@ export default function ImageSlicer() {
                     backdropFilter: 'blur(3.12424px)',
                     borderRadius: '19.5265px',
                     cursor: 'pointer',
-                    zIndex: 50, // Assurez-vous que le z-index est suffisamment élevé pour être au-dessus de tous les autres éléments
                 }}
             ></div>
         );
     };
 
     return (
-        <div style={{ width: '100%', height: '100%', paddingTop: '20px' }}>
+        <div style={{ width: '100%', height: '100%', paddingTop: '20px', position: 'relative', zIndex: 1 }}>
             <ReactCompareSlider
-                boundsPadding={0}
-                handle={<CustomHandle />}
-                itemOne={
-                    <>
-                        <div style={{ position: 'relative', height: '100%', textAlign: 'center' }}>
-                            <img
-                                src="/images/PepeSilver.png"
-                                alt="Image one"
-                                style={{ width: '100%' }}
-                            />
-                            <img
-                                src="/images/headPepeSilver.png"
-                                alt="Image one"
-                                style={{
-                                    position: 'absolute',
-                                    bottom: 0,
-                                    left: '50%',
-                                    transform: 'translateX(-50%)',
-                                    maxWidth: '50%', 
-                                    zIndex: 2, 
-                                    filter: 'grayscale(100%)'
-                                }}
-                            />
-                       
-                        </div>
-                    </>
-                }
-                itemTwo={
-                <>
-                  <div style={{ position: 'relative', height: '100%', textAlign: 'center' }}>
-                            <img
-                                src="/images/PepePurple.png"
-                                alt="Image one"
-                                style={{ width: '100%' }}
-                            />
-                            <img
-                                src="/images/headPepePurple.png"
-                                alt="Image one"
-                                style={{
-                                    position: 'absolute',
-                                    bottom: 0,
-                                    left: '50%',
-                                    transform: 'translateX(-50%)',
-                                    maxWidth: '100%', 
-                                    zIndex: 2, 
-                                }}
-                            />
-                       
-                        </div>     
-                </>
-                
-                }
 
-             
+                handle={<CustomHandle />}
+                itemTwo={
+                    <div
+                        style={{
+                            clipPath: `polygon(calc(${sliderPosition}% - 0.5px) 0, 100% 0, 100% 100%, calc(${sliderPosition}% - 0.5px)  100%)`,
+                            position: 'relative',
+                            height: '100%',
+                            textAlign: 'center',
+                            backgroundSize: 'cover',
+                            width: '100%',
+                        }}
+                    >
+                        <img
+                            src="/images/PepePurple.png"
+                            alt="Image one"
+                            style={{ width: '100%' }}
+                        />
+                        <img
+                            src="/images/headPepePurple.png"
+                            alt="Image one"
+                            style={{
+                                position: 'absolute',
+                                bottom: 0,
+                                left: '50%',
+                                transform: 'translateX(-50%)',
+                                maxWidth: '100%',
+                                zIndex: 2,
+                            }}
+                        />
+                    </div>
+                }
+                itemOne={
+                    <div
+                        style={{
+                            clipPath: `polygon(0 0, 0 100%, calc(${sliderPosition}%) 100%, calc(${sliderPosition}%) 0)`,
+
+                            position: 'relative',
+                            height: '100%',
+                            textAlign: 'center',
+
+                            width: '100%',
+                        }}
+                    >
+                        <img
+                            src="/images/PepeSilver.png"
+                            alt="Image two"
+                            style={{ width: '100%' }}
+                        />
+                        <img
+                            src="/images/headPepeSilver.png"
+                            alt="Image two"
+                            style={{
+                                position: 'absolute',
+                                bottom: 0,
+                                left: '50%',
+                                transform: 'translateX(-50%)',
+                                maxWidth: '46%',
+                                zIndex: 2,
+                                filter: 'grayscale(100%)',
+                            }}
+                        />
+                    </div>
+                }
                 position={sliderPosition}
+                onPositionChange={handleSliderChange}
                 portrait={false}
                 onlyHandleDraggable={false}
                 changePositionOnHover={false}
                 style={{
                     flexGrow: 1,
                     width: '100%',
-                    height: '100%'
+                    height: '100%',
                 }}
             />
             <div
-                                style={{
-                                    position: 'absolute',
-                                    left: '50%', 
-                                    top: '80%',
-                                    transform: 'translate(-50%, -50%)',
-                                    width: '90%',
-                                    height: '90%', 
-                                    borderRadius: '50%',
-                                    background: 'radial-gradient(circle, #5227FF 0%, rgba(97, 0, 255, 0) 80%)', // Utilisation d'un dégradé radial pour former un cercle
-
-                                    opacity: 0.4,
-                                    filter: 'blur(100px)',
-                                    zIndex: 1,
-                                    pointerEvents: 'none', 
-                                }}
-                            />
+                style={{
+                    position: 'absolute',
+                    left: '50%',
+                    top: '80%',
+                    transform: 'translate(-50%, -50%)',
+                    width: '100%',
+                    height: '100%',
+                    borderRadius: '50%',
+                    background: 'radial-gradient(circle, #5227FF 0%, rgba(97, 0, 255, 0) 80%)',
+                    opacity: 0.4,
+                    filter: 'blur(100px)',
+                    zIndex: 1,
+                    pointerEvents: 'none',
+                }}
+            />
         </div>
     );
 }
